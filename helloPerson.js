4c65c6afada359f942859e81df7e719c9ac93315
SaviMohan <smoh944@aucklanduni.ac.nz>
2016-08-06 13:35:51 +1200

    Add project files and folders

---
+++ helloPerson.js
@@ -0,0 +1,6 @@
+function hello(person) {
+    return "Hello, " + person;
+}
+var user = "Deep Seaa";
+//document.body.innerHTML = greeter(user);   
+hello(23823);
---
+++ helloPerson.ts
@@ -0,0 +1,8 @@
+function hello(person: string) {
+    return "Hello, " + person;
+}
+
+var user = "Deep Seaa";
+
+//document.body.innerHTML = greeter(user);   
+hello(23823);
